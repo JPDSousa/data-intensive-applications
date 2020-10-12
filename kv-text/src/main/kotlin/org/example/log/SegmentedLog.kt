c493c12e118b8be6cbf301a1e9423886e4c15d3c
João Sousa <jpd.sousa@campus.fct.unl.pt>
2020-10-12 09:26:54 +0100

    Use binary search to find offsets in segments

--- kv-text/src/main/kotlin/org/example/log/SegmentedLog.kt
+++ kv-text/src/main/kotlin/org/example/log/SegmentedLog.kt
@@ -1,61 +1,29 @@
 package org.example.log
 
 import java.nio.file.Path
-import java.util.*
+import java.util.Collections.binarySearch
+import kotlin.math.abs
 
 class SegmentedLog(path: Path,
                    // 1 MB
-                   private val segmentSize: Long = 1024 * 1024): Log {
+                   segmentSize: Long = 1024 * 1024): Log {
 
     private val factory = SegmentFactory(path)
-    private val segments = mutableListOf(factory.createSegment())
-    private var closedOffset = 0L
-
-    private fun openSegment(): Log {
-
-        val lastSegment = segments.last()
-
-        if (lastSegment.len >= segmentSize) {
-            // TODO add log
-            closedOffset += lastSegment.len
-            val openSegment = factory.createSegment()
-            segments.add(openSegment)
-            return openSegment
-        }
-        // TODO add log
-        return lastSegment
-    }
-
-    private fun segmentsFrom(offset: Long): List<Log> {
-
-        var remaining = offset
-        val segments = LinkedList<Log>()
-
-        for (segment in this.segments) {
-            if (remaining <= 0L) {
-                segments.add(segment)
-            } else if (remaining < segment.len) {
-                segments.add(SubSegmentLog(remaining, segment))
-            }
-            // if remaining is bigger then len then we don't want this segment
-            remaining -= segment.len
-        }
-        return segments
-    }
+    private val segments = Segments(factory, segmentSize)
 
     override fun append(line: String): Long {
-        val openSegment = openSegment()
-        // openSegment() must be called before closedOffset, as that method updates the variable
-        return closedOffset + openSegment.append(line)
+        val openSegment = this.segments.openSegment()
+        // openSegment() must be called before closedOffset, as that method might close another segment
+        return segments.closedOffset() + openSegment.append(line)
     }
 
     override fun appendAll(lines: List<String>): List<Long> = lines.map { append(it) }
 
-    override fun lines(offset: Long): Sequence<String> = segmentsFrom(offset)
+    override fun lines(offset: Long) = segments.from(offset)
             .flatMap { it.lines() }
             .asSequence()
 
-    override fun linesWithOffset(offset: Long): Sequence<Pair<Long, String>> = segmentsFrom(offset)
+    override fun linesWithOffset(offset: Long) = segments.from(offset)
             .flatMap { it.linesWithOffset() }
             .asSequence()
 }
@@ -75,4 +43,51 @@ private class SubSegmentLog(private val offset: Long, private val log: Log): Log
 
     override fun linesWithOffset(offset: Long): Sequence<Pair<Long, String>> = log.linesWithOffset(this.offset +
             offset)
+}
+
+private class Segments(private val factory: SegmentFactory, private val segmentSize: Long): Iterable<SingleFileLog> {
+
+    private val offsets = mutableListOf(0L)
+    private val segments = mutableListOf(factory.createSegment())
+
+    fun from(offset: Long): List<Log> {
+
+        if (offset == 0L) {
+            return segments
+        }
+
+        val binarySearch = binarySearch(offsets, offset)
+        val targetIndex = if (binarySearch < 0) abs(binarySearch) - 2 else binarySearch
+
+        val targetOffset = offsets[targetIndex]
+        if (targetOffset == offset) {
+            return segments.subList(targetIndex, segments.size)
+        }
+        val subSegment = SubSegmentLog(offset - targetOffset, segments[targetIndex])
+
+        if (targetIndex == segments.size - 1) {
+            return listOf(subSegment)
+        }
+
+        return listOf(subSegment) + segments.subList(targetIndex + 1, segments.size)
+    }
+
+    fun closedOffset() = offsets.last()
+
+    fun openSegment(): Log {
+
+        val lastSegment = segments.last()
+
+        if (lastSegment.len >= segmentSize) {
+            // TODO add log
+            offsets.add(offsets.last() + lastSegment.len)
+            val openSegment = factory.createSegment()
+            segments.add(openSegment)
+            return openSegment
+        }
+        // TODO add log
+        return lastSegment
+    }
+
+    override fun iterator() = segments.iterator()
 }
