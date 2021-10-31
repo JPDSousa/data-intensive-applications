99fd93ec6fd950c491c4f167bdc70b7df8bbb73e
João Sousa <jps.liaf@gmail.com>
2021-10-31 12:49:39 +0000

    Add future work references

--- src/main/kotlin/org/example/kv/lsm/SegmentMergeStrategy.kt
+++ src/main/kotlin/org/example/kv/lsm/SegmentMergeStrategy.kt
@@ -1,5 +1,6 @@
 package org.example.kv.lsm
 
+// TODO implement size-tiered and leveled compressing operations
 interface SegmentMergeStrategy<K, V> {
 
     fun merge(segmentsToCompact: List<Segment<K, V>>): List<Segment<K, V>>
